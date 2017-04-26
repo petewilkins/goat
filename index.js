50e255f8a05fb5d5f336ecef208306244dd30858
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-26 16:03:58 +0100

    refactored index.js to remove tests

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 window.onload = function(){
   var list = new List();
-  list.createNote("This is our very first note");
-  list.createNote("This is our very second note");
-  list.createNote("This is our very third note");
   new NoteController(list);
 }
