bc01b16d78a96b8d7e6410824991a80fb84f736a
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-04-26 10:32:09 +0100

    Added new tests for listView

--- noteListView.js
+++ noteListView.js
@@ -7,8 +7,14 @@
   ListView.prototype.displayNotes = function() {
     var historyArray = this.list.history
     var arrayLength = historyArray.length;
-    for (var i = 0; i < arrayLength; i++) {
-      return "<ul><li><div> " + historyArray[i] + " </div></li></ul>";
+    var string = []
+    if (arrayLength === 0) {
+      return "You have no note love.";
+    } else {
+      for (var i = 0; i < arrayLength; i++) {
+        string.push("<ul><li><div> " + historyArray[i] + " </div></li></ul>");
+      }
+      return string.join();
     }
   }
 
--- noteListViewSpec.js
+++ noteListViewSpec.js
@@ -9,4 +9,26 @@ function testTakesAModelAsArgument() {
 
 };
 
+function testTakesModelWithAnEmptyArrayAsArgument() {
+
+  var listDoubleEmpty = {
+    history: []
+  }
+
+  var listView = new ListView(listDoubleEmpty);
+  assert.isEq(listView.displayNotes(), "You have no note love.")
+
+};
+
+function testTakesModelWithMultipleNotesAsArgument() {
+
+  var listDoubleMultiple = {
+    history: ['note1', 'note2', 'note3']
+  }
+
+  var listView = new ListView(listDoubleMultiple)
+  assert.isEq(listView.displayNotes(), "<ul><li><div> note1 </div></li></ul>" + "," + "<ul><li><div> note2 </div></li></ul>" + "," + "<ul><li><div> note3 </div></li></ul>")
+};
 testTakesAModelAsArgument();
+testTakesModelWithAnEmptyArrayAsArgument();
+testTakesModelWithMultipleNotesAsArgument();
