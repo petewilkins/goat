b67593e639f9b20e824f2c5a885edc866da33380
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-04-26 10:57:21 +0100

    Change naming tests for listView

--- noteListView.js
+++ noteListView.js
@@ -7,14 +7,14 @@
   ListView.prototype.displayNotes = function() {
     var historyArray = this.list.history
     var arrayLength = historyArray.length;
-    var string = []
+    var listHtmlArray = []
     if (arrayLength === 0) {
       return "You have no note love.";
     } else {
       for (var i = 0; i < arrayLength; i++) {
-        string.push("<ul><li><div> " + historyArray[i] + " </div></li></ul>");
+        listHtmlArray.push("<ul><li><div> " + historyArray[i] + " </div></li></ul>");
       }
-      return string.join();
+      return listHtmlArray.join();
     }
   }
 
