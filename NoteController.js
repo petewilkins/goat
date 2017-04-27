50d262a19a2885e868e85e696bfa0d607d5a7617
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-27 11:44:07 +0100

    intercepted submit event from create note form

--- NoteController.js
+++ NoteController.js
@@ -11,6 +11,11 @@
     targetDiv.innerHTML = ourListView.displayNotes();
   }
 
+  var newNoteButton = document.getElementById('addnotebutton');
+  newNoteButton.addEventListener("click", function(){
+    console.log(document.getElementById('newnote').value);
+    event.preventDefault();
+  });
 
 exports.NoteController = NoteController;
 
